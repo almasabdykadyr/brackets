2c642a33efbfa6108095bb237549d47a41f021c7
Almas Abdykadyr <almasabdykadir@gmail.com>
2022-08-12 23:51:11 +0600

    feat: implement task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const stack = [];
+
+  for (let i = 0; i < str.length; i++) {
+    const current = str[i];
+    const config = bracketsConfig.find((iter) => iter.includes(current));
+   
+    if (current === config[1] && stack[stack.length - 1] === config[0]) {
+      stack.pop();
+    } else stack.push(current);
+  }
+ 
+  return !stack.length;
 }
